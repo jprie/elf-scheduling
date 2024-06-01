bbfae2107894a04a813a92cfcebde0d7244b62e7
Johannes Priebsch <johannes@priebsch.at>
2024-06-01 11:58:07 +0200

    reorder Task parameters to compTime, deadline, period

--- src/main/java/domain/Task.java
+++ src/main/java/domain/Task.java
@@ -1,13 +1,20 @@
 package domain;
 
+/**
+ *
+ * @param name
+ * @param computationTime ticks per period
+ * @param deadline
+ * @param optionalPeriod
+ */
 public record Task(
         String name,
-        int deadline,
         int computationTime,
+        int deadline,
         int optionalPeriod
 ) {
 
-    public Task(String name, int deadline, int computationTime) {
+    public Task(String name, int computationTime, int deadline) {
 
         this(name, deadline, computationTime, 0);
     }
