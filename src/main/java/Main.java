14c8503d81910f0622567775c72c0875bfe09415
Johannes Priebsch <johannes@priebsch.at>
2024-06-01 12:01:32 +0200

    use new parameter order for tasks

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,13 +9,13 @@ public class Main {
         // example from https://en.wikipedia.org/wiki/Earliest_deadline_first_scheduling
 
         var tasks = List.of(
-                new Task("T0", 13, 5, 20),
-                new Task("T1", 7, 3, 11),
-                new Task("T2", 6, 4, 10),
+                new Task("T0", 5, 13, 20),
+                new Task("T1", 3, 7, 11),
+                new Task("T2", 4, 6, 10),
                 new Task("T3", 1, 1, 20)
         );
 
-        Scheduler.settMax(20);
+//        Scheduler.setTMax(20);
         var schedule = new Scheduler().schedule(tasks);
 
         System.out.println(schedule);
